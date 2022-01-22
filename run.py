61bd01641d037b82e61c934c18bed7b60b74fe54
Rea <rmrea@kenya-airways.com>
2022-01-23 00:06:14 +0300

    add user password validation

--- run.py
+++ run.py
@@ -118,46 +118,97 @@ def display_credentials():
 
 # ****************************** main() ***************************************
 def main():
-    print("Welcome to Pass Locker.This applications enables you to:\ncreate, manage and generate new accounts passwords")
-    print("*"*100)
-    print("What would you like to do... ")
+    print(f"{'*'*24} PASS-LOCKER WEBSITE {'*'*24}")
+    print("Welcome to Pass Locker.This applications enables you to:\ncreate, manage and generate new accounts passwords\n")
     while True:
-        print("Use these short codes: cc - create account, lc -login to your userAccounts, ex -exit the contact list")
-        short_code = input()
-        if short_code == 'cc':
-            print("Please Create User Name and Password\n *******************************")
-            print("Create a User Name ...")
+        print(f"{'*'*25} USER ACCOUNTs {'*'*25}")
+        print("What would you like to do?. (CHOOSE NUMBER CODES)")
+        print("1)CREATE ACCOUNT\n2)LOGIN\n3)EXISTING ACCOUNTs\n4)EXIT\n")
+        short_code = int(input())
+        if short_code == 1:
+            print("Please Create User Name and Password")
+            print("USER NAME: ")
             username = input()
-            print("Create a Password ...")
+            print("PASSWORD: ")
             password = input()
 
             save_account(create_account(username,password))
-            print(f"'{username}' account has been created")
+            print(f"'{username}' account has been created\n")
 
-        elif short_code == 'lc':
-            print("Enter your User Name ...")
+        elif short_code == 2:
+            print(f"{'*'*25} LOGIN {'*'*25}")
+            print("Enter Your Login Information")
+            print("USER NAME: ")
             username = input()
-            if account_exists(username):
-                print("Enter your Password ...")
-                password = input()
-                if find_account(username).password == password:
-                    print("Your existing userAccounts")
-                    print(display_accounts())
-                    print("choose codes: ac - to add userAccounts, dc - delete userAccounts")
-                    ac = input()
+            print("PASSWORD: ")
+            password = input()
 
-                else:
-                    print("invalid password")
+            if account_exists(username) and find_account(username).password == password:
+
+                while True:
+                    print(f"Welcome {username}, Please select:")
+                    print("1)CREATE NEW PAGE PASSWORD.. e.g twitter\n2)DELETE PAGE PASSWORD\n3)DISPLAY EXISTING PASSWORDs\n4)EXIT\n")
+                    user_log = int(input())
+
+                    if user_log == 1:
+                        print(f"{'*'*25} SAVE PAGE PASSWORDs {'*'*25}")
+                        print("Enter Page Name and Password: e.g twitter")
+                        print("PAGE: ")
+                        page_name = input()
+                        print("PASSWORD: ")
+                        password = input()
+
+                        # save new password
+                        save_credential(create_credential(page_name,password))
+                        print(f"page saved\n")
+
+                    elif user_log == 2:
+                        print(f"{'*'*25} DELETE PASSWORDs {'*'*25}")
+                        print(f"Enter Page you want to delete")
+                        print(f"PAGE")
+                        page_name = input()
+                        if credential_exists(page_name):
+                            page_name = (page_name)
+                            delete_credential(page_name)
+                            # delete page passwords
+                            print(f"page deleted\n")
+                        else:
+                            print(f"page NOT FOUND\n")
+                    
+                    elif user_log == 3:
+                        print(f"{'*'*25} DISPLAY PASSWORDs {'*'*25}")
+                        if display_credentials():
+                            for cred in display_credentials():
+                                print(f"{cred.page_name}:{cred.password}")
+                        else:
+                            print(f"NO PASSWORD FOUND\n")
+                    
+                    elif user_log == 4:
+                        break
+                    
+                    else:
+                        print("Invalid Entry")
+
             else:
-                print(f"'{username}' acount NOT FOUND")
+                print(f"User Name '{username}' NOT FOUND")
+                print(f"Wrong UserName OR Password\n")
 
 
-        elif short_code =='ex':
+        elif short_code == 3:
+            print(f"{'*'*25} DISPLAY EXISTING ACCOUNTS {'*'*25}")
+            if display_accounts():
+                for acc in display_accounts():
+                    print(f"{acc.username}\n")
+                
+            else:
+                print("NO ACCOUNTs FOUND\n")
+        
+        elif short_code == 4:
             print("Thank you ... Bye")
             break
 
         else:
-            print("Invalid Entry")
+            print("Invalid Entry\n")
 
 
 
