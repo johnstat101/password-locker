79db9d0a84db1fad1011b83d9f8a7058551aec71
Rea <rmrea@kenya-airways.com>
2022-01-22 15:34:39 +0300

    add user interface

--- run.py
+++ run.py
@@ -2,13 +2,26 @@
 from credentials import Credentials
 
 def main():
-    print(
-        """
-        This applications enables you to:
-        create, manage and generate new accounts passwords
-        """
-        )
-    print()
+    print("This applications enables you to:\ncreate, manage and generate new accounts passwords")
+    print("*"*20)
+    print("Welcome to pass locker. What would you like to do... ")    
+    while True:
+        print("Use these short codes: cc - create account, lc -login to your credentials, ex -exit the contact list")
+        short_code = input()
+        if short_code == 'cc':
+            print("Choose a User Name ...")
+            username = input()
+        elif short_code == 'lc':
+            print("Enter your User Name ...")
+            username = input()
+        elif short_code =='ex':
+            print("Thank you ... Bye")
+            break
+        
+        else:
+            print("Invalid Entry")
+
+
 
 if __name__ == '__main__':
     main()
