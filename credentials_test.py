4e80f2989d2c6f1bd770434ec7a4dc3c718bf96a
Rea <rmrea@kenya-airways.com>
2022-01-22 11:55:33 +0300

    test if the credentials object is initialized correctly

--- credentials_test.py
+++ credentials_test.py
@@ -14,3 +14,10 @@ class TestCredentials(unittest.TestCase):
         Set up method to run before each test cases.
         """
         self.new_credential = Credentials("johnKim", "1234") #create an object of type Credentials
+
+    def test_init(self):
+        """
+        Test whether the object is initialized correctly
+        """
+        self.assertEqual(self.new_credential.username,"johnKim")
+        self.assertEqual(self.new_credential.password,"1234")
